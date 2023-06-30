729900d80a9fcb55ee2055aea3a08bda1cd7d4bd
clone <ilyes.esma@outlook.fr>
2023-06-30 20:06:39 +0200

    Mon commit de rebase

--- ConsolSmallApp/Sudoku.cs
+++ ConsolSmallApp/Sudoku.cs
@@ -19,6 +19,10 @@ namespace ConsolSmallApp
             Console.WriteLine("La je teste la situation ou tout le monde commit comme un idiot");
             Console.WriteLine("regardons ce qu'il se passe !");
             Console.WriteLine("blablabla je suis quelqu'un d'autre qui fais des test");
+            Console.WriteLine("Tu pues du cul Nan c'est toi qui put du cul");
+            Console.WriteLine("est-ce qu el git merge est meilleur que le git rebase");
+            Console.WriteLine("Le conflit est je n'en sais rien");            
+            Console.WriteLine("On va voir si ça marche Let's go and let's see");
             Console.WriteLine("On regarde ce qu'il se passe quand on rebase");
             Console.WriteLine("est-ce que ça va encore déconner");
             Console.WriteLine("Je ne le sais pas");            
