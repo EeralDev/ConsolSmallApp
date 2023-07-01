123491f018231afc231ee46bd3782e5b75fdbeb3
EeralDev <ilyes.esma@outlook.fr>
2023-07-01 15:39:18 +0200

    Ajout d'un petit lien dans une branche a part dans le cadre de mon exercice de git

--- ConsolSmallApp/Sudoku.cs
+++ ConsolSmallApp/Sudoku.cs
@@ -9,8 +9,10 @@ namespace ConsolSmallApp
     //Nouveaux jeux a développer : Sudoku
     //Petit site web pour s'inspirer et commencer tranquillement le dev du sudoku
     //https://www.programmingalgorithms.com/algorithm/sudoku/
+    //https://www.codeproject.com/Articles/12473/Sudoku-Game-in-C
     internal class Sudoku
     {
+        
         //Du coup la je fais juste un petit commit pour décaler ma branche Sudoku
         // J'essaie d'ajouter un truc pour voir si l'autre voit encore la branche qui n'est plus sensé exister !
         int[] gameTab;
