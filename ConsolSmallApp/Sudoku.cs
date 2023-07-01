434d82fc7287383c19acb600da3ffb91d7dc2160
clone <ilyes.esma@outlook.fr>
2023-07-01 14:59:13 +0200

    Petit commit dans le cadre du test du rebase de branche

--- ConsolSmallApp/Sudoku.cs
+++ ConsolSmallApp/Sudoku.cs
@@ -9,6 +9,7 @@ namespace ConsolSmallApp
     //Nouveaux jeux a développer : Sudoku
     internal class Sudoku
     {
+        //Du coup la je fais juste un petit commit pour décaler ma branche Sudoku
         int[] gameTab;
         private int _difficulty;
 
